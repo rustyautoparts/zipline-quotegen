e97585afa648ac30f00412e3eeb27e94ee25b68b
Russ Otto <unplugged68@gmail.com>
2015-11-02 14:43:36 -0600

    First commit

---
+++ quote-gen.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {
+var quotes = [
+  {
+    "quote": "The cleaner and nicer the program, the faster it's going to run. And if it doesn't, it'll be easy to make it fast.",
+    "author": "Joshua Bloch",
+    "source": "An interview by Peter Seibel in <em>Coders At Work</em>"
+  },
+  {
+    "quote": "You‘ve got to start with the customer experience and work back toward the technology - not the other way around.",
+    "author": "Steve Jobs",
+    "source": "WWDC 1997"
+  },
+  {
+    "quote": "The effective exploitation of his powers of abstraction must be regarded as one of the most vital activities of a competent programmer.",
+    "author": "Edsger W. Dijkstra",
+    "source": "<em>The Humble Programmer</em>"
+  },
+  {
+    "quote": "Computers are man's attempt at designing a cat: it does whatever it wants, whenever it wants, and rarely ever at the right time.",
+    "author": "EMCIC",
+    "source": "<em>Keenspot Elf Life Forum</em>"
+  },
+  {
+    "quote": "If you lie to the computer, it will get you.",
+    "author": "Perry Farrar",
+    "source": "Attributed to Farrar by John Bentley in 'Bumper-Sticker Computer Science,' <em>Communications of the ACM</em>, Vol 28. Nr. 9, p. 899"
+  },
+  {
+    "quote": "There is no programming language, no matter how structured, that will prevent programmers from making bad programs.",
+    "author": "Larry Flon",
+    "source": "'On research in structured programming'. <em>SIGPLAN Not.</em>, 10(10), pp.16-17"
+  },
+  {
+    "quote": "To me programming is more than an important practical art. It is also a gigantic undertaking in the foundations of knowledge.",
+    "author": "Grace Hopper",
+    "source": "Quoted in <em>Management and the Computer of the Future</em>"
+  },
+  {
+    "quote": "Programming: when the ideas turn into the real things.",
+    "author": "Maciej Kaczmarek",
+    "source": ""
+  },
+  {
+    "quote": "Premature optimization is the root of all evil.",
+    "author": "Donald Knuth",
+    "source": "'Structured Programming with Goto Statements,' <em>Computing Surveys</em>"
+  },
+  {
+    "quote": "He who hasn't hacked assembly language as a youth has no heart. He who does as an adult has no brain.",
+    "author": "John Moore",
+    "source": ""
+  },
+  {
+    "quote": "Languages shape the way we think, or don't.",
+    "author": "Erik Naggum",
+    "source": ""
+  },
+  {
+    "quote": "Computer Science is embarrassed by the computer.",
+    "author": "Alan Perlis",
+    "source": "'Epigrams on Programming'"
+  },
+  {
+    "quote": "Prolonged contact with the computer turns mathematicians into clerks and vice versa.",
+    "author": "Alan Perlis",
+    "source": "'Epigrams on Programming'"
+  },
+  {
+    "quote": "Structured Programming supports the law of the excluded muddle.",
+    "author": "Alan Perlis",
+    "source": "'Epigrams on Programming'"
+  },
+  {
+    "quote": "There are two ways to write error-free programs; only the third one works.",
+    "author": "Alan Perlis",
+    "source": "'Epigrams on Programming'"
+  },
+  {
+    "quote": "Software and cathedrals are much the same &#x2013; first we build them, then we pray.",
+    "author": "Sam Redwine",
+    "source": ""
+  },
+  {
+    "quote": "Why bother with subroutines when you can type fast?",
+    "author": "Vaughn Rokosz",
+    "source": ""
+  },
+  {
+    "quote": "Real Programmers always confuse Christmas and Halloween because Oct31 == Dec25.",
+    "author": "Andrew Rutherford",
+    "source": ""
+  },
+  {
+    "quote": "Don't get suckered in by the comments &#x2026 they can be terribly misleading.",
+    "author": "Dave Storer",
+    "source": ""
+  },
+  {
+    "quote": "The three chief virtues of a programmer are: Laziness, Impatience and Hubris.",
+    "author": "Larry Wall",
+    "source": "<em>Programming in Perl</em>"
+  },
+  {
+    "quote": "One day my daughter came in, looked over my shoulder at some Perl 4 code, and said, \"What is that, swearing?\"",
+    "author": "Larry Wall",
+    "source": ""
+  },
+  {
+    "quote": "Weinberg's Second Law: If builders built buildings the way programmers wrote programs, then the first woodpecker that came along would destroy civilization.",
+    "author": "Gerald Weinberg",
+    "source": "Cited by Murali Chemuturi in <em>Mastering Software Quality Assurance: Best Practices, Tools and Technique for Software Developers</em>"
+  },
+  {
+    "quote": "The main activity of programming is not the origination of new independent programs, but in the integration, modification, and explanation of existing ones.",
+    "author": "Terry Winograd",
+    "source": "'Beyond Programming Languages', <em>Artificial Intelligence & Software Engineering</em>"
+  }
+];
+console.log(quotes);
+  var shuffledQuotes = shuffleQuotes(quotes);
+  var counter = 0;
+  displayQuote();
+  $("#new-quote").on("click", function() {
+    displayQuote();
+  });
+  function displayQuote() {
+    counter++;
+    var quote = shuffledQuotes[counter].quote;
+    var author = shuffledQuotes[counter].author;
+    var source = shuffledQuotes[counter].source;
+    var tweetUri = encodeURI(quote);
+    $("#quote")
+      .removeClass()
+      .html(quote);
+    if (quote.length > 550) {
+      $("#quote").addClass("long-quote");
+    }
+    $("#author").html(author);
+    $("#source").html(source);
+    $("#tweet-quote").attr("href", "http://twitter.com/intent/tweet?text=" + tweetUri);
+  }
+  function shuffleQuotes(list) {
+    var remainingQuotes = list.length, quoteHolder, randomNum;
+    while (remainingQuotes) {
+      nextNum = Math.floor(Math.random() * remainingQuotes--);
+      quoteHolder = list[remainingQuotes];
+      list[remainingQuotes] = list[quoteHolder];
+      list[nextNum] = quoteHolder;
+    }
+    return list;
+  }
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+});
